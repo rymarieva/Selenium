88812de81330b8546007ca71648c296dd6b9d163
Olga Rymarieva <oli4ka.olka@outlook.com>
2019-03-18 21:23:57 +0200

    тест по ДЗ

---
+++ OldQalight.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium
+{
+    class OldQalight
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // тамаут для дозагрузке элементов и драйвер через 0.5 сек повтаряет операцию, пытается найти элемент пока он "проресовывается" 
+            driver.Navigate().GoToUrl("http://old.qalight.com.ua/zapisatsya-na-kursy/");
+           
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Category("Smoke")]
+        [Test]
+        public void SmokeTest()
+        {
+            //Arrange
+            string expectedElementLocator = ".alert.alert-error";
+
+            //Act
+            IWebElement course = driver.FindElement(By.CssSelector("[name='_7c8289bf6b8e80c1749ef54ab01b92b8']"));
+            SelectElement courseDropdown = new SelectElement(course);
+            courseDropdown.SelectByIndex(3);
+
+
+            IWebElement surnameField = driver.FindElement(By.Id("z_sender0"));
+            surnameField.SendKeys("Surname");
+            //driver.FindElement(By.Id("z_sender0")).SendKeys("surname"); аналогия предыдущех строк
+
+            IWebElement nameField = driver.FindElement(By.Id("z_text1"));
+            nameField.SendKeys("Name");
+
+            IWebElement phoneField = driver.FindElement(By.Id("z_text0"));
+            phoneField.SendKeys("0661112233");
+
+            IWebElement emailField = driver.FindElement(By.Id("z_sender1"));
+            emailField.SendKeys("rymarieva@gmail.com");
+
+            IWebElement skypeField = driver.FindElement(By.Id("z_text2"));
+           // skypeField.SendKeys("testskype");
+
+            IWebElement sourseInfo = driver.FindElement(By.CssSelector("[name='_e926ba2b2813f56de8fc13877057e908']"));
+            SelectElement sourseInfoDropdown = new SelectElement(sourseInfo);
+            sourseInfoDropdown.SelectByIndex(4);
+
+            IWebElement submitButton = driver.FindElement(By.CssSelector("[type=submit]"));
+            submitButton.Click();
+
+            //IWebElement succesElement = driver.FindElement(By.CssSelector(".alert.alert-succes"));
+
+            IWebElement succesElement = driver.FindElement(By.CssSelector(".alert.alert-error"));
+
+            // Assert
+            Assert.True(IsElementPresent(expectedElementLocator, driver), $"Expected element {expectedElementLocator} is not found"); 
+        }
+
+        public bool IsElementPresent(string cssSelector, IWebDriver driver)
+        {
+            var element = driver.FindElements(By.CssSelector(cssSelector));
+            if (element.Count == 1)
+            {
+                return true;
+            }
+            else if (element.Count == 1)
+            {
+                return false;
+            }
+            else
+            {
+                throw new Exception("Unexpected error");
+            }
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,10 +8,46 @@ namespace Selenium
  
     public class UITests
     {
-     [Test]
+        IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ukr.net/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // тамаут для дозагрузке элементов и драйвер через 0.5 сек повтаряет операцию, пытается найти элемент пока он "проресовывается" 
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
         public void TestWebDriverMethods()
         {
-            IWebDriver driver = new ChromeDriver();
+            var windowHandle = driver.CurrentWindowHandle; //id открытого окна
+
+            System.Console.WriteLine(windowHandle);
+
+            IWebElement element = driver.FindElement(By.CssSelector("test css")); // найти элемент по селектору
+
+            //var elements = driver.FindElements(By.CssSelector("test multi css")); //найти элементы
+
+            var allWindowHandles = driver.WindowHandles; //коллекция открытых окон, порядок элементов может не совпадать с порядком открытия окон
+
+            driver.SwitchTo().Window(windowHandle);
+            // driver.Manage() - полезная штука
+            //Logs работа с логами консоли, Cookies - работа с куками, 
+            //Window - работа с окном браузера (скрины, изменения размеров)
+            //Timeouts().
+            //Timeouts().ImplicitWait - 
+            //IJavaScriptExetutor  - для віполнения скриптоов в своих тестах
+            //SwitchTo().Alert() - работа с всплівающими окнами
+            //SwitchTo().Frame - перейти на фрейм, парент фрейм - вернуться на 1 фрейм вверх, SwitchTo().DefaultContent - на главную страницу если есть фреймы
+
+            
         }
     }
 }
